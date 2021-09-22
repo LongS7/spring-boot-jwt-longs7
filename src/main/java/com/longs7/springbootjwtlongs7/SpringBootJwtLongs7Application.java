6f91c349af59fcf29b7296091ef51c399c1d8b07
longs7 <longkeyvic1@gmail.com>
2021-09-22 11:02:30 +0700

    update config, add register api

--- src/main/java/com/longs7/springbootjwtlongs7/SpringBootJwtLongs7Application.java
+++ src/main/java/com/longs7/springbootjwtlongs7/SpringBootJwtLongs7Application.java
@@ -2,8 +2,9 @@ package com.longs7.springbootjwtlongs7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SpringBootJwtLongs7Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJwtLongs7Application.class, args);
--- src/main/java/com/longs7/springbootjwtlongs7/controller/UserController.java
+++ src/main/java/com/longs7/springbootjwtlongs7/controller/UserController.java
@@ -3,6 +3,8 @@ package com.longs7.springbootjwtlongs7.controller;
 import com.longs7.springbootjwtlongs7.entity.User;
 import com.longs7.springbootjwtlongs7.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,9 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public void registerUser(@RequestBody User user) {
+    User registerUser(@RequestBody User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
 
+        return userService.saveUser(user);
     }
 }
